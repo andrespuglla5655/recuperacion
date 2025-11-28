67d7e60c655056f22fae2ce498f7083f4e66e068
andrespuglla5655 <air.puglla@yavirac.edu.ec>
2025-11-28 16:42:21 -0500

    todo funcional

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def home():
         </head>
         <body>
             <div class="container">
-                <h1>🚀 Examen</h1>
+                <h1>🚀 Examen byron</h1>
                 <p class="version">{version_info}</p>
                 <div class="info">
                     <h3>📊 Información del Sistema:</h3>
